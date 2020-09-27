871413a05b151c10bf3e516f5333f91590608673
Nuttachai Kulthammanit <6370086021@student.chula.ac.th>
2020-09-28 01:26:47 +0700

    change enci

--- src/containers/pages/Statistics/Graph.js
+++ src/containers/pages/Statistics/Graph.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "../../../config/axios";
 
 function Graph() {
-    const [currentChromosome, setCurrentChromosome] = useState("Autosome");
-    const [currentLocus, setCurrentLocus] = useState("Amelogenin");
-    const [locusList, setLocusList] = useState({ a: [], x: [], y: [] });
+  const [currentChromosome, setCurrentChromosome] = useState("Autosome");
+  const [currentLocus, setCurrentLocus] = useState("Amelogenin");
+  const [locusList, setLocusList] = useState({});
 
-    return (
-        <div>
+  useEffect(() => {
+    axios
+      .get("/loci/all")
+      .then((res) =>
+        setLocusList({
+          a: res.data.aList,
+          x: res.data.xList,
+          y: res.data.yList,
+        })
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-        </div>
-    );
+  return <div></div>;
 }
 
 export default Graph;
